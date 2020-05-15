3e01634e65d6adf35e58b606c174a7e90563c0f6
Jan Rubel <jasiekrubel@gmail.com>
2020-05-16 00:18:56 +0200

    Added Model, Cleaned ViewModel

---
+++ MiniTC/Model/CopyingModel.cs
@@ -0,0 +1,79 @@
+﻿using MiniTC.Properties;
+using System;
+using System.IO;
+using System.Linq;
+
+
+namespace MiniTC.Model
+{
+    class CopyingModel
+    {
+        public void Copy(string source, string target)
+        {
+            var attribute = File.GetAttributes(source);
+            if (attribute.HasFlag(FileAttributes.Directory))
+            {
+                target = Path.Combine(target, Path.GetFileName(source));
+                DirectoryCopy(source, target);
+            }
+            else
+            {
+                FileCopy(source, target);
+            }
+        }
+
+        #region Auxiliary functions
+        private void FileCopy(string source, string target)
+        {
+            if (Directory.GetFiles(target).Select(x => Path.GetFileName(x)).Contains(Path.GetFileName(source)))
+            {
+                int count = Directory.GetFiles(target).Select(x => Path.GetFileName(x)).Where(x => x.StartsWith(Path.GetFileNameWithoutExtension(source))).Count();
+                string fileName = Path.GetFileNameWithoutExtension(source) + " - " + Resources.FileCopy + count + Path.GetExtension(source);
+                target = Path.Combine(target, fileName);
+            }
+            else
+            {
+                target = Path.Combine(target, Path.GetFileName(source));
+            }
+            try
+            {
+                File.Copy(source, target);
+            }
+            catch (UnauthorizedAccessException) { return; }
+        }
+
+        private void DirectoryCopy(string source, string target)
+        {
+            var dir = new DirectoryInfo(source);
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException();
+            DirectoryInfo[] dirs;
+            try
+            {
+                dirs = dir.GetDirectories();
+            }
+            catch (UnauthorizedAccessException) { return; }
+
+            if (!Directory.Exists(target))
+                Directory.CreateDirectory(target);
+            else
+            {
+                int count = Directory.GetDirectories(Directory.GetDirectoryRoot(target)).Where(x => x.StartsWith(target)).Count();
+                target = Path.Combine(Path.GetDirectoryName(target), Path.GetFileNameWithoutExtension(target) + " - " + Resources.FileCopy + count);
+                Directory.CreateDirectory(target);
+            }
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                string path = Path.Combine(target, file.Name);
+                file.CopyTo(path);
+            }
+            foreach (var subdir in dirs)
+            {
+                string path = Path.Combine(target, subdir.Name);
+                DirectoryCopy(subdir.FullName, path);
+            }
+        }
+        #endregion
+    }
+}
--- MiniTC/ViewModel/MainViewModel.cs
+++ MiniTC/ViewModel/MainViewModel.cs
@@ -1,17 +1,15 @@
-﻿using System;
-
-namespace MiniTC.ViewModel
+﻿namespace MiniTC.ViewModel
 {
     using BaseClass;
     using MiniTC.Properties;
-    using System.Globalization;
+    using Model;
     using System.IO;
-    using System.Linq;
     using System.Windows.Input;
 
     class MainViewModel : ViewModelBase
     {
         #region Properties and fields
+        private CopyingModel _model;
         private PanelTCViewModel _leftPanel;
         public PanelTCViewModel LeftPanel
         {
@@ -33,7 +31,7 @@ namespace MiniTC.ViewModel
             //System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US"); // Default is English
             _leftPanel = new PanelTCViewModel();
             _rightPanel = new PanelTCViewModel();
-
+            _model = new CopyingModel();
             Copy = new RelayCommand(CopyExecute, CopyCanExecute);
             LeftSelectionChange = new RelayCommand(LeftSelectionChangeExecute, arg => true);
             RightSelectionChange = new RelayCommand(RightSelectionChangeExecute, arg => true);
@@ -60,18 +58,10 @@ namespace MiniTC.ViewModel
                 target = Path.GetFullPath(LeftPanel.CurrentPath);
             }
 
-            //checking if path is directory or file
-            var attribute = File.GetAttributes(source);
-            if (attribute.HasFlag(FileAttributes.Directory))
-            {
-                target = Path.Combine(target, Path.GetFileName(source));
-                DirectoryCopy(source, target);
-            }
-            else
-            {
-                FileCopy(source, target);
-            }
-            UpdateCurrentPathContents();
+           _model.Copy(source, target); // Model usage
+
+
+            UpdateCurrentPathContents(); // Updating view
         }
 
         private bool CopyCanExecute(object obj)
@@ -99,58 +89,6 @@ namespace MiniTC.ViewModel
         {
             LeftPanel.UpdateCurrentPathContent();
             RightPanel.UpdateCurrentPathContent();
-        }
-
-        private void FileCopy(string source, string target)
-        {
-            if (Directory.GetFiles(target).Select(x => Path.GetFileName(x)).Contains(Path.GetFileName(source)))
-            {
-                int count = Directory.GetFiles(target).Select(x => Path.GetFileName(x)).Where(x => x.StartsWith(Path.GetFileNameWithoutExtension(source))).Count();
-                string fileName = Path.GetFileNameWithoutExtension(source) + " - " + Resources.FileCopy + count + Path.GetExtension(source);
-                target = Path.Combine(target, fileName);
-            }
-            else
-            {
-                target = Path.Combine(target, Path.GetFileName(source));
-            }
-            try
-            {
-                File.Copy(source, target);
-            }
-            catch (UnauthorizedAccessException) { return; }
-        }
-
-        private void DirectoryCopy(string source, string target)
-        {
-            var dir = new DirectoryInfo(source);
-            if (!dir.Exists)
-                throw new DirectoryNotFoundException();
-            DirectoryInfo[] dirs;
-            try
-            {
-                dirs = dir.GetDirectories();
-            }
-            catch (UnauthorizedAccessException) { return; }
-
-            if (!Directory.Exists(target))
-                Directory.CreateDirectory(target);
-            else
-            {
-                int count = Directory.GetDirectories(Directory.GetDirectoryRoot(target)).Where(x => x.StartsWith(target)).Count();
-                target = Path.Combine(Path.GetDirectoryName(target), Path.GetFileNameWithoutExtension(target) + " - " + Resources.FileCopy + count);
-                Directory.CreateDirectory(target);
-            }
-            var files = dir.GetFiles();
-            foreach (var file in files)
-            {
-                string path = Path.Combine(target, file.Name);
-                file.CopyTo(path);
-            }
-            foreach (var subdir in dirs)
-            {
-                string path = Path.Combine(target, subdir.Name);
-                DirectoryCopy(subdir.FullName, path);
-            }
         }
         #endregion
     }
